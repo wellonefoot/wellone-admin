6dd52780aa08b5ddb2874d7e971dd987e4cc5f2c
wellonefoot <wellonefoot09@gmail.com>
2026-08-28 12:57:13 +0530

    Delete js directory

--- js/admin-config.js
+++
@@ -1,5 +0,0 @@
-const ADMIN_CONFIG = {
-  supabaseUrl: 'https://wnavzhrkwgnegjdetdno.supabase.co',
-  supabaseAnonKey: 'sb_publishable_RbnMrDlHfEijBiejcRNPUg_mop2bqgM',
-  storageBucket: 'product-images'
-};
